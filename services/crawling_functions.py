645c6c81b1fdaf005f43fbf99e5991fdb3887dd4
管鐘 <86526364+RogelioKG@users.noreply.github.com>
2023-11-22 03:23:02 +0800

    Update crawling_functions.py

--- services/crawling_functions.py
+++ services/crawling_functions.py
@@ -4,21 +4,19 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException
 
 
-def fetch_info(block: WebElement) -> tuple[str, str, int] | bool:
+def fetch_info(block: WebElement) -> tuple[str, str, int]:
     """從一筆資料中抓取標題、敘述與價格
 
     Parameters
     ----------
-    + `block` (WebElement) : 一筆資料
+    + `block` (WebElement) : 一塊網頁元素
 
     Returns
     -------
-    + (tuple[str, str, int] | bool)
-        - 1. 找得到符合特定元素的資料，回傳 tuple
-            - title (str) : 標題
-            - desc (str) : 敘述
-            - price (int) : 價格
-        - 2. 找不到符合特定元素的資料，回傳 False
+    + (tuple[str, str, int])
+        - title (str) : 標題
+        -  desc (str) : 敘述
+        - price (int) : 價格
 
     Exceptions
     ----------
